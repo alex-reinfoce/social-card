368b01033fc02fd06b7b6371a3d53e2d2588565f
alex-reinfoce <alex.lichangnan@gmail.com>
2025-01-04 10:05:44 +0800

    Upgrade pnpm lockfile to version 9.0 and refactor app/page.tsx to include InputBox component with state management.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/component/button";
+"use client";
+import { InputBox } from "@/component/input-box";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+
   return (
-    <div className="flex">
-      <Button />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <InputBox value={value} onChange={setValue} />
     </div>
   );
 }
