1432de3f0bc92ea6625e53c4dbd6a4d14e8d7459
alex-reinfoce <alex.lichangnan@gmail.com>
2025-01-06 02:38:30 +0800

    Update Next.js configuration to enable TypeScript and ESLint build error ignoring; add dynamic color background dependency; modify global styles to use transparent backgrounds for animated circles; refactor layout to include dynamic color background component.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
 };
 
 export default nextConfig;
